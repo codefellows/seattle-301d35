bd0801872fe80ac3b095419edf4e5cf427b1e802
Sam Hamm <sam@codefellows.com>
2018-06-21 09:54:32 -0700

    Adds iPad images from Classes 12 and 13

--- 13-middleware/demo-kilovolt-revenge/server.js
+++ 13-middleware/demo-kilovolt-revenge/server.js
@@ -8,25 +8,25 @@ const requestProxy = require('express-request-proxy'); // REVIEW: We've added a 
 const PORT = process.env.PORT || 3000;
 const app = express();
 // const conString = 'postgres://USERNAME:PASSWORD@HOST:PORT';
-const conString = 'postgres://localhost:5432'; // TODO: Don't forget to set your own conString
+const conString = 'postgres://localhost:5432/kilovolt'; // TODO: Don't forget to set your own conString
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', err => console.error(err));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('./public'));
 
 function proxyGitHub(request, response) {
   console.log('Routing GitHub request for', request.params[0]);
   (requestProxy({
     url: `https://api.github.com/${request.params[0]}`,
-    headers: {Authorization: `token ${process.env.GITHUB_TOKEN}`}
+    headers: { Authorization: `token ${process.env.GITHUB_TOKEN}` }
   }))(request, response);
 }
 
-app.get('/new', (request, response) => response.sendFile('new.html', {root: './public'}));
-app.get('/admin', (request, response) => response.sendFile('admin.html', {root: './public'}));
+app.get('/new', (request, response) => response.sendFile('new.html', { root: './public' }));
+app.get('/admin', (request, response) => response.sendFile('admin.html', { root: './public' }));
 app.get('/github/*', proxyGitHub);
 
 // REVIEW: This is a new route to find a specific instance of an article record from the DB.
@@ -37,31 +37,29 @@ app.get('/articles/find', (request, response) => {
             WHERE ${request.query.field}=$1`
 
   client.query(sql, [request.query.val])
-  .then(result => response.send(result.rows))
-  .catch(console.error);
+    .then(result => response.send(result.rows))
+    .catch(console.error);
 })
 
 // REVIEW: This is a new route for gathering all of the unique categories from our articles table
 app.get('/categories', (request, response) => {
   client.query(`SELECT DISTINCT category FROM articles;`)
-  .then(result => response.send(result.rows))
-  .catch(console.error);
+    .then(result => response.send(result.rows))
+    .catch(console.error);
 })
 
 app.get('/articles', (request, response) => {
   client.query(`
     SELECT * FROM articles
     INNER JOIN authors
-      ON articles.author_id=authors.author_id;`
-  )
-  .then(result => response.send(result.rows))
-  .catch(console.error);
+      ON articles.author_id=authors.author_id;`)
+    .then(result => response.send(result.rows))
+    .catch(console.error);
 });
 
 app.post('/articles', function(request, response) {
   client.query(
-    'INSERT INTO authors(author, "authorUrl") VALUES($1, $2) ON CONFLICT DO NOTHING',
-    [request.body.author, request.body.authorUrl],
+    'INSERT INTO authors(author, "authorUrl") VALUES($1, $2) ON CONFLICT DO NOTHING', [request.body.author, request.body.authorUrl],
     function(err) {
       if (err) console.error(err)
       queryTwo()
@@ -70,8 +68,7 @@ app.post('/articles', function(request, response) {
 
   function queryTwo() {
     client.query(
-      `SELECT author_id FROM authors WHERE author=$1`,
-      [request.body.author],
+      `SELECT author_id FROM authors WHERE author=$1`, [request.body.author],
       function(err, result) {
         if (err) console.error(err)
         queryThree(result.rows[0].author_id)
@@ -83,8 +80,7 @@ app.post('/articles', function(request, response) {
     client.query(
       `INSERT INTO
       articles(author_id, title, category, "publishedOn", body)
-      VALUES ($1, $2, $3, $4, $5);`,
-      [
+      VALUES ($1, $2, $3, $4, $5);`, [
         author_id,
         request.body.title,
         request.body.category,
@@ -104,47 +100,42 @@ app.put('/articles/:id', (request, response) => {
     UPDATE authors
     SET author=$1, "authorUrl"=$2
     WHERE author_id=$3
-    `,
-    [request.body.author, request.body.authorUrl, request.body.author_id]
-  )
-  .then(() => {
-    client.query(`
+    `, [request.body.author, request.body.authorUrl, request.body.author_id])
+    .then(() => {
+      client.query(`
       UPDATE articles
       SET author_id=$1, title=$2, category=$3, "publishedOn"=$4, body=$5
       WHERE article_id=$6
-      `,
-      [
+      `, [
         request.body.author_id,
         request.body.title,
         request.body.category,
         request.body.publishedOn,
         request.body.body,
         request.params.id
-      ]
-    )
-  })
-  .then(() => response.send('Update complete'))
-  .catch(console.error);
+      ])
+    })
+    .then(() => response.send('Update complete'))
+    .catch(console.error);
 });
 
 app.delete('/articles/:id', (request, response) => {
   client.query(
-    `DELETE FROM articles WHERE article_id=$1;`,
-    [request.params.id]
-  )
-  .then(() => response.send('Delete complete'))
-  .catch(console.error);
+      `DELETE FROM articles WHERE article_id=$1;`, [request.params.id]
+    )
+    .then(() => response.send('Delete complete'))
+    .catch(console.error);
 });
 
 app.delete('/articles', (request, response) => {
   client.query('DELETE FROM articles')
-  .then(() => response.send('Delete complete'))
-  .catch(console.error);
+    .then(() => response.send('Delete complete'))
+    .catch(console.error);
 });
 
 loadDB();
 
-app.get('/*', (request, response) => response.sendFile('index.html', {root: './public/'}));
+app.get('/*', (request, response) => response.sendFile('index.html', { root: './public/' }));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
 
@@ -155,34 +146,31 @@ function loadAuthors() {
   fs.readFile('./public/data/hackerIpsum.json', (err, fd) => {
     JSON.parse(fd.toString()).forEach(ele => {
       client.query(
-        'INSERT INTO authors(author, "authorUrl") VALUES($1, $2) ON CONFLICT DO NOTHING',
-        [ele.author, ele.authorUrl]
-      )
-      .catch(console.error);
+          'INSERT INTO authors(author, "authorUrl") VALUES($1, $2) ON CONFLICT DO NOTHING', [ele.author, ele.authorUrl]
+        )
+        .catch(console.error);
     })
   })
 }
 
 function loadArticles() {
   client.query('SELECT COUNT(*) FROM articles')
-  .then(result => {
-    if(!parseInt(result.rows[0].count)) {
-      fs.readFile('./public/data/hackerIpsum.json', (err, fd) => {
-        JSON.parse(fd.toString()).forEach(ele => {
-          client.query(`
+    .then(result => {
+      if (!parseInt(result.rows[0].count)) {
+        fs.readFile('./public/data/hackerIpsum.json', (err, fd) => {
+          JSON.parse(fd.toString()).forEach(ele => {
+            client.query(`
             INSERT INTO
             articles(author_id, title, category, "publishedOn", body)
             SELECT author_id, $1, $2, $3, $4
             FROM authors
             WHERE author=$5;
-          `,
-            [ele.title, ele.category, ele.publishedOn, ele.body, ele.author]
-          )
-          .catch(console.error);
+          `, [ele.title, ele.category, ele.publishedOn, ele.body, ele.author])
+              .catch(console.error);
+          })
         })
-      })
-    }
-  })
+      }
+    })
 }
 
 function loadDB() {
@@ -192,10 +180,9 @@ function loadDB() {
       author_id SERIAL PRIMARY KEY,
       author VARCHAR(255) UNIQUE NOT NULL,
       "authorUrl" VARCHAR (255)
-    );`
-  )
-  .then(loadAuthors)
-  .catch(console.error);
+    );`)
+    .then(loadAuthors)
+    .catch(console.error);
 
   client.query(`
     CREATE TABLE IF NOT EXISTS
@@ -206,8 +193,7 @@ function loadDB() {
       category VARCHAR(20),
       "publishedOn" DATE,
       body TEXT NOT NULL
-    );`
-  )
-  .then(loadArticles)
-  .catch(console.error);
+    );`)
+    .then(loadArticles)
+    .catch(console.error);
 }
